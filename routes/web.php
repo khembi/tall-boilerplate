07dbbdb5f9b2f30c16a5f5cf4e19afa7420630f2
Khem Tsogt <khem.tsogt@gmail.com>
2023-09-12 13:21:31 +1000

    socialite installed

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['prefix' => 'auth', 'as' => 'auth.'], function () {
+    Route::group(['prefix' => 'google', 'as' => 'google.'], function () {
+        Route::get('/redirect', [AuthController::class, 'googleRedirect'])->name('redirect');
+        Route::get('/callback', [AuthController::class, 'googleCallback'])->name('callback');
+    });
 });
