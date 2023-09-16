8cea3cfa44d1f6c87bea3654fab9e73585cf9d63
Khem Tsogt <khem.tsogt@gmail.com>
2023-09-16 19:48:22 +1000

    avatar added to navbar

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,6 +23,10 @@
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                            <div class="flex-shrink-0 mr-2">
+                                <img class="inline-block h-8 w-8 rounded-full" src="{{ Auth::user()->profile_picture_url }}" alt="">
+                            </div>
+
                             <div>{{ Auth::user()->name }}</div>
 
                             <div class="ml-1">
